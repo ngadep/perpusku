50ef7dacc0ce39f112d0900f4c47fdebe960a54e
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-09-18 00:47:37 +0700

    View.Pengembalian

---
+++ Source/View.Pengembalian.pas
@@ -0,0 +1,24 @@
+unit View.Pengembalian;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TFrmPengembalian = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  FrmPengembalian: TFrmPengembalian;
+
+implementation
+
+{$R *.dfm}
+
+end.
