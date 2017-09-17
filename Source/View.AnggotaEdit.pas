9c52dad83c2ad68c1a89a0eefbcf5b292bf6f75b
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-09-18 00:46:54 +0700

    View.AnggotaEdit

---
+++ Source/View.AnggotaEdit.pas
@@ -0,0 +1,24 @@
+unit View.AnggotaEdit;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TFrmAnggotaEdit = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  FrmAnggotaEdit: TFrmAnggotaEdit;
+
+implementation
+
+{$R *.dfm}
+
+end.
