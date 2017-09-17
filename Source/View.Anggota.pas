c1c2f3ea5b03fd511fa7c4f619e9ac3d2217b843
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-09-18 00:46:39 +0700

    View.Anggota

---
+++ Source/View.Anggota.pas
@@ -0,0 +1,24 @@
+unit View.Anggota;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TFrmAnggota = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  FrmAnggota: TFrmAnggota;
+
+implementation
+
+{$R *.dfm}
+
+end.
