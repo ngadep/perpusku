ef9c77d1829ed17cb03814357ba472e0cbcafd19
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-09-18 00:47:19 +0700

    View.Peminjaman

---
+++ Source/View.Peminjaman.pas
@@ -0,0 +1,24 @@
+unit View.Peminjaman;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TFrmPeminjaman = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  FrmPeminjaman: TFrmPeminjaman;
+
+implementation
+
+{$R *.dfm}
+
+end.
