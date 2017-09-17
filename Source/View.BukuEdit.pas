fe7590290a4e1bf6ac2dd20fc06fd2b5a819a791
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-09-18 00:47:09 +0700

    View.BukuEdit

---
+++ Source/View.BukuEdit.pas
@@ -0,0 +1,24 @@
+unit View.BukuEdit;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TFrmBukuEdit = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  FrmBukuEdit: TFrmBukuEdit;
+
+implementation
+
+{$R *.dfm}
+
+end.
